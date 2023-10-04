e4f291393de1dcc6d18020b9ae42fa5a0d66c7d4
Bor Hodošček <dev@bor.space>
2023-10-04 21:40:15 +0900

    chore: move patterns

--- src/dm_annotations/patterns.py
+++ src/dm_annotations/patterns.py
@@ -1,13 +1,13 @@
 import json
 
-with open("modality-patterns.json") as f:
+with open("resources/modality-patterns.json") as f:
     modality_patterns = json.loads(f.read())
 
 # TODO ものの as 文中接続表現
-with open("connectives-patterns.json") as f:
+with open("resources/connectives-patterns.json") as f:
     connectives_patterns = json.loads(f.read())
 
-with open("connectives-regexes.json") as f:
+with open("resources/connectives-regexes.json") as f:
     connectives_regexes = json.loads(f.read())
 
 if __name__ == "__main__":
